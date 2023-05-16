861169a3fbb04050cacccd7e05e34d4b93130a4d
fernando <fernando.hfl@protonmail.com>
2023-05-16 20:10:59 -0300

    feature intents diff

--- controllers/exporter.py
+++ controllers/exporter.py
@@ -24,3 +24,21 @@ class Exporter:
             self._logger.log(str(e))
             print(e)
             raise
+
+    def export_diff_intents(self, diff_dict):
+        try:
+            export_list = []
+            for intent in list(diff_dict.keys()):
+                for example in diff_dict[intent]:
+                    export_list.append([intent, example])
+
+            if not os.path.exists(self._export_directory):
+                os.makedirs(self._export_directory)
+
+            df = pd.DataFrame(export_list)
+            df.to_csv(self._export_directory + 'intents_diff.csv', index=False, header=False)
+            return export_list  
+        except Exception as e:
+            self._logger.log(str(e))
+            print(e)
+            raise
